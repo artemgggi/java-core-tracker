bf68ddd15d64f5b79a6c4a41c9024fd8a308ca66
artemgggi <artem.ivanchikov.99@mail.ru>
2021-10-16 12:36:19 +0300

    creating object task

---
+++ src/main/java/ru/job4j/tracker/Error.java
@@ -0,0 +1,29 @@
+package ru.job4j.tracker;
+
+public class Error {
+	private boolean active;
+	private int status;
+	private String message;
+
+	public Error() {
+	}
+
+	public Error(boolean active, int status, String message) {
+		this.active = active;
+		this.status = status;
+		this.message = message;
+	}
+
+	public void printInfo() {
+		System.out.println("Active: " + active);
+		System.out.println("Status: " + status);
+		System.out.println("Message: " + message);
+	}
+
+	public static void main(String[] args) {
+		Error error = new Error();
+		error.printInfo();
+		Error error1 = new Error(true,1,"Power problem");
+		error1.printInfo();
+	}
+}
