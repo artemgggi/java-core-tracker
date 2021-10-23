678c3c751c0594378dc59dad4826d238fcc5586b
Artem <artem.ivanchikov.99@mail.ru>
2021-10-23 12:17:54 +0300

    polimorfism using interface task

---
+++ src/main/java/ru/job4j/oop/polymorfism/example/DomesticAnimal.java
@@ -0,0 +1,7 @@
+package ru.job4j.oop.polymorfism.example;
+
+public class DomesticAnimal extends Animal {
+    public void liveOnFarm() {
+        System.out.println(nameClass + "Живет на ферме");
+    }
+}
