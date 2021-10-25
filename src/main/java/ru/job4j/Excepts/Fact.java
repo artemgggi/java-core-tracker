0b8b2ac4300be4544cf2770c749f3349f421a0ff
artemgggi <artem.ivanchikov.99@mail.ru>
2021-10-25 15:11:37 +0300

    trow exceptions task

---
+++ src/main/java/ru/job4j/Excepts/Fact.java
@@ -0,0 +1,18 @@
+package ru.job4j.Excepts;
+
+public class Fact {
+    public static void main(String[] args) {
+        new Fact().calc(-1);
+    }
+
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("число должно быть больше нуля");
+        }
+        int rsl = 1;
+        for (int i = 0; i <= n; i++) {
+            rsl *= i;
+        }
+        return rsl;
+    }
+}
