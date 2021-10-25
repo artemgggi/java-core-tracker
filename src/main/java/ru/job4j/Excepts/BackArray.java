ddb658db61921d15746d99ce0e309f93a539306d
artemgggi <artem.ivanchikov.99@mail.ru>
2021-10-25 14:49:19 +0300

    exceptions task

---
+++ src/main/java/ru/job4j/Excepts/BackArray.java
@@ -0,0 +1,16 @@
+package ru.job4j.Excepts;
+
+import java.util.Arrays;
+
+public class BackArray {
+    public static void main(String[] args) {
+        String[] names = {"Petr", "Ivan", "Nik", "Vasya"};
+        int middle = names.length / 2;
+        for (int i = 1; i <= middle; i++) {
+            String temp = names[i];
+            names[i] = names[names.length - i];
+            names[names.length - i] = temp;
+        }
+        System.out.println(Arrays.toString(names));
+    }
+}
