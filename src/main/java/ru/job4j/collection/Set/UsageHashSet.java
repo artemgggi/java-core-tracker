b4e2bbfd18fe9b8a2274554b1da97080ff1cd86e
artemgggi <artem.ivanchikov.99@mail.ru>
2021-10-29 10:06:02 +0300

    HashSet adding elements task

---
+++ src/main/java/ru/job4j/collection/Set/UsageHashSet.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection.Set;
+
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class UsageHashSet {
+    public static void main(String[] args) {
+        HashSet<String> autos = new HashSet<String>();
+        autos.add("Lada");
+        autos.add("BMW");
+        autos.add("Volvo");
+        autos.add("Toyota");
+        for (String auto : autos) {
+            System.out.println(auto);
+        }
+    }
+}
