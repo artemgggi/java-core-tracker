0a423e40ef75e2fa76869fe29cd97515a5523508
artemgggi <artem.ivanchikov.99@mail.ru>
2021-11-10 17:51:19 +0300

    adding departments task

---
+++ src/main/java/ru/job4j/sorting/departmentsTask/SortedDepartments.java
@@ -0,0 +1,12 @@
+package ru.job4j.sorting.departmentsTask;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class SortedDepartments implements Comparator<Departments> {
+
+    @Override
+    public int compare(Departments left, Departments right) {
+        return ;
+    }
+}
