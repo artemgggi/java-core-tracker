cd1615dfa81925fe0ab4621e08578a579ff66678
Artem Ivanchikov <artem.ivanchikov.99@mail.ru>
2022-01-21 12:33:12 +0300

    Create my own realisation list based on dinamic array

---
+++ src/main/java/ru/job4j/List/DinamicListTask/SimpleArrayList.java
@@ -0,0 +1,38 @@
+package ru.job4j.List.DinamicListTask;
+
+import java.util.Iterator;
+
+public class SimpleArrayList<T> implements List<T> {
+    public SimpleArrayList(T i) {
+    }
+
+    @Override
+    public void add(T value) {
+
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        return null;
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+}
