1b1856321ef11ce8a2bf1ee85effc4c40ffd4142
Artem Ivanchikov <artem.ivanchikov.99@mail.ru>
2022-01-24 09:38:45 +0300

    Refactoring

--- src/main/java/ru/job4j/List/DinamicListTask/SimpleArrayList.java
+++ src/main/java/ru/job4j/List/DinamicListTask/SimpleArrayList.java
@@ -1,14 +1,29 @@
 package ru.job4j.List.DinamicListTask;
 
+import ru.job4j.oop.objects.Array;
+
+import java.util.Arrays;
 import java.util.Iterator;
 
 public class SimpleArrayList<T> implements List<T> {
-    public SimpleArrayList(T i) {
+
+    private T[] container;
+
+    private int size;
+
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        this.container = (T[]) new Object[capacity];
     }
 
     @Override
     public void add(T value) {
-
+        if (size == modCount) {
+            container = Arrays.copyOf(container, container.length * 2);
+        }
+        container[size] = value;
+        size++;
     }
 
     @Override
@@ -18,7 +33,13 @@ public class SimpleArrayList<T> implements List<T> {
 
     @Override
     public T remove(int index) {
-        return null;
+        System.arraycopy(container,
+                        index + 1,
+                        container,
+                        index,
+                        container.length - index - 1);
+        container[container.length - 1] = null;
+        return container;
     }
 
     @Override
@@ -28,11 +49,21 @@ public class SimpleArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return 0;
+        return null;
     }
 
     @Override
-    public Iterator iterator() {
-        return null;
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public T next() {
+                return null;
+            }
+        };
     }
 }
