675b727beb09979d6fcbf63e09ddb98c5bd7965b
Artem Ivanchikov <artem.ivanchikov.99@mail.ru>
2022-02-01 12:40:59 +0300

    EvenNumberFile task refactor

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -3,20 +3,31 @@ package ru.job4j.io;
 import java.io.FileInputStream;
 
 public class EvenNumberFile {
+    public static boolean isNumeric(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
+        StringBuilder text = new StringBuilder();
         try (FileInputStream in = new FileInputStream("even.txt")) {
-            StringBuilder text = new StringBuilder();
             int read;
-            boolean rsl = false;
             while ((read = in.read()) != -1) {
                 text.append((char) read);
             }
-            String[] lines = text.toString().split(System.lineSeparator());
-            for (String line : lines) {
-                System.out.println(line);
-            }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+        String[] lines = text.toString().split(System.lineSeparator());
+        for (String line : lines) {
+            if (EvenNumberFile.isNumeric(line)
+                    && Integer.parseInt(line) % 2 == 0) {
+            System.out.println(line);
+            }
         }
     }
 }
