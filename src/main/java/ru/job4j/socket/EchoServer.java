665f3e23fb181923ea283a6d740ba3811a1488c1
artemgggi <artem.ivanchikov.99@mail.ru>
2022-02-06 13:56:35 +0300

    creating client request validating on server

--- src/main/java/ru/job4j/socket/EchoServer.java
+++ src/main/java/ru/job4j/socket/EchoServer.java
@@ -16,7 +16,11 @@ public class EchoServer {
 				     BufferedReader in = new BufferedReader(
 						     new InputStreamReader(socket.getInputStream()))) {
 					out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-					for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
+					String str = in.readLine();
+					if (str.contains("Bye")) {
+						server.close();
+					}
+					for (str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
 						System.out.println(str);
 					}
 					out.flush();
