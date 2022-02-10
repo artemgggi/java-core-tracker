d89af4d3ef674bb19aa1783cc1b459b49cfacb4c
artemgggi <artem.ivanchikov.99@mail.ru>
2022-02-10 11:06:46 +0300

    adding SLF plugin

---
+++ src/main/java/ru/job4j/UsageLog4jSLF.java
@@ -0,0 +1,20 @@
+package ru.job4j;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UsageLog4jSLF {
+
+	private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+	public static void main(String[] args) {
+		String log4jConfPath = "src/main/resources/log4j.properties.properties";
+		PropertyConfigurator.configure(log4jConfPath);
+		LOG.trace("trace message");
+		LOG.debug("debug message");
+		LOG.info("info message");
+		LOG.warn("warn message");
+		LOG.error("error message");
+	}
+}
