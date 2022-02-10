cbd04ece152b49944559a01b0a7c918648c37a3b
artemgggi <artem.ivanchikov.99@mail.ru>
2022-02-10 10:57:19 +0300

    refactoring

--- src/main/java/ru/job4j/UsageLog4j.java
+++ src/main/java/ru/job4j/UsageLog4j.java
@@ -1,15 +1,17 @@
 package ru.job4j;
 
-import org.apache.log4j.BasicConfigurator;
+
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 public class UsageLog4j {
 
 	private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
 
 	public static void main(String[] args) {
-		BasicConfigurator.configure();
+		String log4jConfPath = "src/main/resources/log4j.properties.properties";
+		PropertyConfigurator.configure(log4jConfPath);
 		LOG.trace("trace message");
 		LOG.debug("debug message");
 		LOG.info("info message");
