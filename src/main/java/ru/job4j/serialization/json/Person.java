9373865fd3c904748fb59ffbf78602da0f87064a
Artem <artem.ivanchikov.99@mail.ru>
2022-02-12 22:40:06 +0300

    Pojo to json simple serialization

--- src/main/java/ru/job4j/serialization/json/Person.java
+++ src/main/java/ru/job4j/serialization/json/Person.java
@@ -3,16 +3,28 @@ package ru.job4j.serialization.json;
 import java.util.Arrays;
 
 public class Person {
-	private final boolean sex;
-	private final int age;
-	private final Contact contact;
-	private final String[] statuses;
+	private  boolean sex;
+	private  int age;
+	private  Contact contact;
+	private  String[] statuses;
+
 
 	public Person(boolean sex, int age, Contact contact, String[] statuses) {
 		this.sex = sex;
 		this.age = age;
 		this.contact = contact;
 		this.statuses = statuses;
+	}
+
+	public Person(boolean sex, int age, Contact contact, String worker, String married) {
+	}
+
+	public boolean getSex() {
+		return sex;
+	}
+
+	public int getAge() {
+		return age;
 	}
 
 	@Override
