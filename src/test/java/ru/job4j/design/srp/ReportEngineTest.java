39f95f42833bb651ebb80aac68105e43a1ff3b18
Artem <artem.ivanchikov.99@mail.ru>
2022-04-07 20:27:00 +0600

    added report task with srp

---
+++ src/test/java/ru/job4j/design/srp/ReportEngineTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.design.srp;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class ReportEngineTest {
+
+    @Test
+    public void whenOldGenerated() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 100);
+        store.add(worker);
+        Report engine = new ReportEngine(store);
+        String expect = "Name; Hired; Fired; Salary;" +
+                System.lineSeparator() +
+                worker.getName() + ";" +
+                worker.getHired() + ";" +
+                worker.getFired() + ";" +
+                worker.getSalary() + ";" +
+                System.lineSeparator();
+        assertThat(engine.generate(em -> true), is(expect));
+    }
+}
